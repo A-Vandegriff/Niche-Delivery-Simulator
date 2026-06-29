5d0d134237d59f00de3b48aa5c77298e5b4493bc
Austin Vandegriff <austinvandegriff@yahoo.com>
2026-06-29 16:08:11 -0400

    created getters and setters for Rig class

--- Main.java
+++ Main.java
@@ -6,9 +6,17 @@ import java.util.Random;
 
 public class Main {
     public static void main(String[] args) { 
-        Field myField = new Field();
+        String[] truckBrands = {"Western Star", "Freightliner"};
+        int[] truckQuantity = {7500, 8900, 10500};
         Random random = new Random();
+        Field myField = new Field();
+        Rig rig = new Rig();
+        
         myField.setAcres(random.nextInt(40,150));
-        System.out.println(myField.getAcres());
+        rig.setBrand(truckBrands[random.nextInt(truckBrands.length)]);
+        rig.setQuantity(truckQuantity[random.nextInt(truckQuantity.length)]);
+        System.out.println("Field for the day: " + myField.getAcres() + " acres");
+        System.out.println("Assigned rig:\n Truck, " + rig.getBrand() + "\n Trailer, " + rig.getQuantity() + " gallons");
+
 	 }
 }
--- classes/rig/Rig.java
+++ classes/rig/Rig.java
@@ -2,11 +2,25 @@ package classes.rig;
 
 public class Rig {
     private String truckBrand;
-    private int trailerAxles;
+    private int trailerQuantity;
     
     public Rig(){
 
 
+    }
+    //setter and getters for truck brand
+    public void setBrand(String truckBrand){
+        this.truckBrand = truckBrand;
+    }
+    public String getBrand(){
+        return truckBrand;
+    }
+    //setter and getters for truck quantity
+    public void setQuantity(int trailerQuantity){
+        this.trailerQuantity = trailerQuantity;
+    }
+    public int getQuantity(){
+        return trailerQuantity;
     }
 
 }
