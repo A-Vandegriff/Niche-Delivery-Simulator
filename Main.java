8c4fcacf38aa27fdd38380398d5f41d00ee34005
Austin Vandegriff <austinvandegriff@yahoo.com>
2026-07-08 13:21:13 -0400

    Randomized round trip

--- Main.java
+++ Main.java
@@ -2,15 +2,28 @@ import classes.farm.Farm;
 import classes.rig.Rig;
 import classes.route.Route;
 import classes.field.Field;
-import java.util.Random;
+/*
+STUFF NEEDED TO FULLY CALCULATE THE TIME OF A JOB
+
+taking the amount of acres to do between all the fields
+the gallons the trailer can hold, amount of trailers
+whether the farm is using a spreader or frac tank (consistant flow or not)
+how the route could impact the flow if multiple trucks
+
+*/
+//work on route, come up with real life situations and ideas that can predict a route 
+//even with stop signs and traffic, maybe use time of the day for busyness etc. 
 
 public class Main {
     public static void main(String[] args) { 
 
         Field myField = new Field();
         Rig rig = new Rig();
+        Route route = new Route();
 
         myField.displayFields(myField.getAllFields());
         rig.randomizeRig();
+        route.roundTrip();
+        route.totRoundTrip();
 	 }
 }
--- classes/route/Route.java
+++ classes/route/Route.java
@@ -1,9 +1,38 @@
 package classes.route;
+import java.util.Random;
+
+
 
 public class Route {
-    private int distanceInMiles;
+    Random random = new Random();
+    private int distanceFarmToField;
+    private int distanceFieldToFarm;
+    private int totRoundTrip;
+    private int[] roundTrip = new int[2];
+    private int gravelMiles;
+    private int pavedMiles;
+    private int gravelSpeedLimit = 30;
+    private int pavedSpeedLimit = 55;
+    private int[] citySpeedLimit = {25,30,35,40,45,50};
+
     public Route(){
         
     }
+    public int[] roundTrip(){
+        distanceFarmToField = random.nextInt(3,11);
+        distanceFieldToFarm = random.nextInt(3,11);
+        roundTrip[0] = distanceFarmToField;
+        roundTrip[1] = distanceFieldToFarm;
+        return roundTrip;
+    }
+    public int totRoundTrip(){
+        totRoundTrip = roundTrip[0] + roundTrip[1];
+        System.out.println("Farm to field: " + roundTrip[0] + " miles\n" + "Field to Farm: " + roundTrip[1] + " miles");
+        System.out.println("Total Miles: " + totRoundTrip);
+
+        return totRoundTrip;
+    }
+
+
 
 }
